187bb5a775f7b10220622b9f22aa2ad7beba33dd
OskarFranck <oskar.franck@gmail.com>
2022-11-18 23:54:39 +0100

    init

---
+++ src/JDBC.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class JDBC {
+    // change this to local database connection string
+    static final String DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT";
+    // change this to database username
+    static final String USER = "guest";
+    static final String PASS = "guest123";
+   // this is just a sample query change to match our sample table.
+    static final String QUERY = "SELECT id, first, last, age FROM Employees";
+
+
+    public static void main(String[] args) {
+        // Open a connection
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY);) {
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print(", Age: " + rs.getInt("age"));
+                System.out.print(", First: " + rs.getString("first"));
+                System.out.println(", Last: " + rs.getString("last"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
