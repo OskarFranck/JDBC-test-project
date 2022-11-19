7c84e0e58860c51f500e12f8b4a2fd17074c9f1d
OskarFranck <oskar.franck@gmail.com>
2022-11-19 10:05:26 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# What to do?
+
+## mySQL
+- Setup up a mySQL DB
+- Crate a table and add some sample data
+##
+## java
+- Download mySQL JDBC https://dev.mysql.com/downloads/connector/j/
+- Unpack the .jar file and find location.
+- Set JDBC path, either globally (windows path variables)
+or in project through intellij (project structure -> libraries -> path to mysql connector)
+
--- src/JDBC.java
+++ src/JDBC.java
@@ -2,26 +2,26 @@ import java.sql.*;
 
 public class JDBC {
     // change this to local database connection string
-    static final String DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/test";
     // change this to database username
-    static final String USER = "guest";
-    static final String PASS = "guest123";
+    static final String USER = "root";
+    static final String PASS = "password";
    // this is just a sample query change to match our sample table.
-    static final String QUERY = "SELECT id, first, last, age FROM Employees";
+    static final String QUERY = "SELECT id, model, year, doors FROM cars";
 
 
     public static void main(String[] args) {
         // Open a connection
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(QUERY);) {
+            ResultSet rs = stmt.executeQuery(QUERY)) {
             // Extract data from result set
             while (rs.next()) {
                 // Retrieve by column name
                 System.out.print("ID: " + rs.getInt("id"));
-                System.out.print(", Age: " + rs.getInt("age"));
-                System.out.print(", First: " + rs.getString("first"));
-                System.out.println(", Last: " + rs.getString("last"));
+                System.out.print(", Age: " + rs.getString("model"));
+                System.out.print(", First: " + rs.getInt("year"));
+                System.out.println(", Last: " + rs.getInt("doors"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
