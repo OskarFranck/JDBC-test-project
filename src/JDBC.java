b98fb24f83abd7cf922ab0a828d0a263e2a20062
OskarFranck <oskar.franck@gmail.com>
2022-11-19 11:23:12 +0100

    split settings and code

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,31 +1,14 @@
 import java.sql.*;
 
+// standard to split the connection part from the functionality, since in a development environment our settings won't be the same.
+// like DB_URL.
 public class JDBC {
     // change this to local database connection string
-    static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    final String DB_URL = "jdbc:mysql://localhost:3306/test";
     // change this to database username
-    static final String USER = "root";
-    static final String PASS = "password";
+    final String USER = "root";
+    final String PASS = "password";
    // this is just a sample query change to match our sample table.
-    static final String QUERY = "SELECT id, model, year, doors FROM cars";
-
-
-    public static void main(String[] args) {
-        // Open a connection
-        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(QUERY)) {
-            // Extract data from result set
-            while (rs.next()) {
-                // Retrieve by column name
-                System.out.print("ID: " + rs.getInt("id"));
-                System.out.print(", Age: " + rs.getString("model"));
-                System.out.print(", First: " + rs.getInt("year"));
-                System.out.println(", Last: " + rs.getInt("doors"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    final String QUERY = "SELECT id, model, year, doors FROM cars";
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.*;
 public class Main {
+    static JDBC db_connection = new JDBC();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // Open a connection
+        try(Connection conn = DriverManager.getConnection(db_connection.DB_URL, db_connection.USER, db_connection.PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(db_connection.QUERY)) {
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print(", Age: " + rs.getString("model"));
+                System.out.print(", First: " + rs.getInt("year"));
+                System.out.println(", Last: " + rs.getInt("doors"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
